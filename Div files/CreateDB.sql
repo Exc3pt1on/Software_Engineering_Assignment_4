cb1e8da707706087b295ba8144709108a34f8d5f
Kim Langvannskås <Kimlangv@hotmail.com>
2024-04-24 19:32:05 +0200

    work on assignment paper and update erwin model

--- Div files/CreateDB.sql
+++ Div files/CreateDB.sql
@@ -1,36 +1,63 @@
+CREATE DATABASE Ineos_Beredskap_Checklists
+GO
+USE Ineos_Beredskap_Checklists
+GO
 
-CREATE TABLE [User]
+
+CREATE TABLE Checklist
 ( 
-	[user_Id]            int  IDENTITY ( 1,1 )  NOT NULL ,
-	[username]           char(7)  NULL ,
-	[password]           char(64)  NULL ,
-	CONSTRAINT [XPKUser] PRIMARY KEY  CLUSTERED ([user_Id] ASC)
+	checklist_Id         int  IDENTITY ( 1,1 )  NOT NULL ,
+	title                char(50)  NULL ,
+	description          char(50)  NULL ,
+	created              datetime  NULL ,
+	updated              datetime  NULL ,
+	sign                 binary  NULL ,
+	shift                char(1)  NULL ,
+	operator_id          int  NULL ,
+	approved             binary  NULL 
 )
 go
 
-CREATE TABLE [Checklist]
+CREATE TABLE Item
 ( 
-	[checklist_Id]       int  IDENTITY ( 1,1 )  NOT NULL ,
-	[title]              char(50)  NULL ,
-	[description]        char(50)  NULL ,
-	[created]            datetime  NULL ,
-	[updated]            datetime  NULL ,
-	[sign]               binary  NULL ,
-	[shift]              char(1)  NULL ,
-	[user_Id]            int  NULL ,
-	CONSTRAINT [XPKChecklist] PRIMARY KEY  CLUSTERED ([checklist_Id] ASC),
-	CONSTRAINT [R_1] FOREIGN KEY ([user_Id]) REFERENCES [User]([user_Id])
+	item_Id              int  IDENTITY ( 1,1 )  NOT NULL ,
+	description          char(50)  NULL ,
+	completed            binary  NULL ,
+	position             char(50)  NULL ,
+	checklist_Id         int  NULL 
 )
 go
 
-CREATE TABLE [Item]
+CREATE TABLE Operator
 ( 
-	[item_Id]            int  IDENTITY ( 1,1 )  NOT NULL ,
-	[description]        char(50)  NULL ,
-	[completed]          binary  NULL ,
-	[position]           char(50)  NULL ,
-	[checklist_Id]       int  NULL ,
-	CONSTRAINT [XPKItems] PRIMARY KEY  CLUSTERED ([item_Id] ASC),
-	CONSTRAINT [R_2] FOREIGN KEY ([checklist_Id]) REFERENCES [Checklist]([checklist_Id])
+	operator_id          int  IDENTITY ( 1,1 )  NOT NULL ,
+	username             char(7)  NULL ,
+	password             char(64)  NULL 
 )
+go
+
+ALTER TABLE Checklist
+	ADD PRIMARY KEY  CLUSTERED (checklist_Id ASC)
+go
+
+ALTER TABLE Item
+	ADD PRIMARY KEY  CLUSTERED (item_Id ASC)
+go
+
+ALTER TABLE Operator
+	ADD PRIMARY KEY  CLUSTERED (operator_id ASC)
+go
+
+
+ALTER TABLE Checklist
+	ADD  FOREIGN KEY (operator_id) REFERENCES Operator(operator_id)
+		ON DELETE NO ACTION
+		ON UPDATE NO ACTION
+go
+
+
+ALTER TABLE Item
+	ADD  FOREIGN KEY (checklist_Id) REFERENCES Checklist(checklist_Id)
+		ON DELETE NO ACTION
+		ON UPDATE NO ACTION
 go
